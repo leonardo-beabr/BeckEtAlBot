c47a84a9893dc4e90415b7ef3a9e53ce53b75d47
Leonardo <leonardo@android-378d0a24ef786165.localdomain>
2020-01-30 17:25:26 -0300

    Added array of messagens and a random method to get it

--- src/components/Slack/SlackFunctions.js
+++ src/components/Slack/SlackFunctions.js
@@ -29,7 +29,8 @@ module.exports = {
     */
     EverydayNotify(params = {'janitors' : Array, 'birthdays': Array}){
         // console.log(params)
-        let currentJanitorPhrase = '', currentBirthdayPhrase = ''
+        let currentJanitorPhrase = '', currentBirthdayPhrase = '', slackMessage = '', 
+            janitor1 = params['janitors'][0]['Slack Id'], janitor2 = params['janitors'][1]['Slack Id']
         //Phrases that can be used
         let janitorPhrases = {
             'portuguese': [],
@@ -50,36 +51,41 @@ module.exports = {
                 currentBirthdayPhrase = birthday[randomLanguage][Math.floor(Math.random() * birthday[randomLanguage].length)]
             }
         }
-        RandomPhrase(janitorPhrases, birthdaysPhrases)
+        // RandomPhrase(janitorPhrases, birthdaysPhrases)
         //Set a array of emojis http://emoji-cheat-sheet.com/
         const emojiList = [":sparkles:", ":boom:", ":muscle:", ":trophy:", ":rocket:", ":hotsprings:"]
-        // console.log('Emoji1: ', emojiList[Math.floor(Math.random()*emojiList.length)])
-        // console.log('Emoji2: ', emojiList[Math.floor(Math.random()*emojiList.length)])
+        const phrases = [
+            `Bom dia! Hoje <@${janitor1}> e <@${janitor2}> estarão auxiliando na organização do nosso ambiente de trabalho`,
+            `Bahhhh gurizada, hoje os Zelas são os <@${janitor1}> <@${janitor2}> dos meu. Eles que vão dar uma girica na cozinha neh!`,
+            `E ai camaradinhas, os zeladores de hoje são <@${janitor1}> e <@${janitor2}>. Vamo dalhe!`
+        ]
+        slackMessage = `${emojiList[Math.floor((Math.random()* emojiList.length))]} ${phrases[Math.floor((Math.random()* phrases.length))]} ${emojiList[Math.floor((Math.random()* emojiList.length))]}`
         //const enMessage = `The janitors of this week are ${users[0]['Name']} and ${users[1]['Name']} from ${users[0]['Start']} to ${users[0]['End']}`
         //Array of messages that the App will send for the channel
-        let ptMessage = [
-            `Os zeladores são <@${params['janitors'][0]['Slack Id']}> e <@${params['janitors'][1]['Slack Id']}>`//,
-            //`O time da zeladoria nesta semana é composta por ${users[0]['Name']} e ${users[1]['Name']}`,
-            //`Hoje quem entra em campo para cuidar da zeladoria é o ${users[0]['Name']} e o ${users[1]['Name']}`,
-            //`A escalação do Dream Team da Zeladoria é: `
-        ]
+        //This will be removed soon
+        // let ptMessage = [
+        //     `Os zeladores são <@${params['janitors'][0]['Slack Id']}> e <@${params['janitors'][1]['Slack Id']}>`//,
+        //     //`O time da zeladoria nesta semana é composta por ${users[0]['Name']} e ${users[1]['Name']}`,
+        //     //`Hoje quem entra em campo para cuidar da zeladoria é o ${users[0]['Name']} e o ${users[1]['Name']}`,
+        //     //`A escalação do Dream Team da Zeladoria é: `
+        // ]
         if(params['birthdays'].length === 1){
-            ptMessage[0] = ptMessage[0]  + ` e o aniversariante do dia é o :tada: ${params['birthdays'][0]['Slack Id']} :tada:`
+            slackMessage = slackMessage  + ` e o aniversariante do dia é o :tada: ${params['birthdays'][0]['Slack Id']} :tada:`
         }
         if(params['birthdays'].length > 1){
-            ptMessage[0] = ptMessage[0] + ' e os anivesariantes do dia são '
+            slackMessage = slackMessage + ' e os anivesariantes do dia são '
             for(let i = 0; i < params['birthdays'].length; i++){
                 if(i === params['birthdays'].length - 1){
-                    ptMessage[0] = ptMessage[0] + ` e ${params['birthdays'][i]['Name']}`
+                    slackMessage = slackMessage + ` e ${params['birthdays'][i]['Name']}`
                 }
                 else{
-                    ptMessage[0] = ptMessage[0] + `${params['birthdays'][i]['Name']}`
+                    slackMessage = slackMessage + `${params['birthdays'][i]['Name']}`
                 }
             }
         }
-        console.log(ptMessage[0])
-        // bot.postMessage('DSH3K8AF3', ptMessage[0], slackParams);
-        bot.postMessage(process.env.CHANNEL, ptMessage[0], slackParams)
+        // console.log(slackMessage)
+        bot.postMessage('DSH3K8AF3', ptMessage[0], slackParams);
+        // bot.postMessage(process.env.CHANNEL, ptMessage[0], slackParams)
     },
     ErrorNotify(error){
         console.log(error)
