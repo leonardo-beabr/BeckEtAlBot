4829be42253bcfba5b77fdb5efe958795124a137
Leonardo <leonardo@android-378d0a24ef786165.localdomain>
2020-02-03 09:18:10 -0300

    Additional information about the weather

--- src/components/Weather/WeatherFunctions.js
+++ src/components/Weather/WeatherFunctions.js
@@ -5,7 +5,20 @@ module.exports = {
             weather.find({search: '88075-120, SC', degreeType: 'C'}, function(err, result) {
                 if(err){console.log(err);reject(err)}
                 else{
-                    const phrase = `A temperatura atual em ${result[0]['location']['name']} é de ${result[0]['current']['temperature']}°C com máxima de ${result[0]['forecast'][1]['high']}°C e mínima de ${result[0]['forecast'][1]['low']}°C.`
+                    let phrase = `A temperatura atual em ${result[0]['location']['name']} é de ${result[0]['current']['temperature']}°C com máxima de ${result[0]['forecast'][1]['high']}°C e mínima de ${result[0]['forecast'][1]['low']}°C.`
+                    if(result[0]['current']['skytext'] === 'Light Rain'){
+                        phrase = phrase + ` Hoje o dia terá chuva de leve. :cloud: :umbrella:.`
+                    }
+                    if(result[0]['current']['skytext'] === 'T-Storms'){
+                        phrase = phrase + ` Teremos chuvas com trovoadas :cloud: :zap:.`
+                    }
+                    if(result[0]['current']['skytext'] === 'Cloudy' || result[0]['current']['skytext'] === 'Mostly Cloudy'){
+                        phrase = phrase + ` O dia de hoje será nublado :cloud:.`
+                    }
+                    if(result[0]['current']['skytext'] === 'Sunny' || result[0]['current']['skytext'] === 'Mostly Sunny'){
+                        phrase = phrase + ` Hoje será um dia ensolarado :sunny:.`
+                    }
+                    console.log(phrase)
                     const weatherInfo = {
                         'location': result[0]['location']['name'],
                         'today': {
